917bf91c5350fad7fcff974ec2c6c3afc2f00c31
takuma-8 <220425@jc-21.jp>
2025-01-30 16:22:33 +0900

    ジャンプスケア

--- Assets/Scenes/newPlayScene/Script/life.cs
+++ Assets/Scenes/newPlayScene/Script/life.cs
@@ -1,26 +1,41 @@
 using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
-public class life : MonoBehaviour
+public class Life : MonoBehaviour
 {
-    void OnTriggerEnter(Collider other)
+    public Image gameOverImage;  // Inspectorで設定
+    public float displayTime = 2.0f;  // 画像表示時間
+
+    private void Start()
     {
-        // Playerタグを持つオブジェクトか確認
+        // 初期状態で画像を非表示にする
+        gameOverImage.gameObject.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
         if (gameObject.CompareTag("Player"))
         {
-            // Zornタグのオブジェクトに触れた場合
             if (other.gameObject.CompareTag("Enemy2") || other.gameObject.CompareTag("Enemy1"))
             {
-                TriggerGameOver();
+                StartCoroutine(TriggerGameOver());
             }
         }
     }
 
-    // ゲームオーバー時にシーン遷移
-    private void TriggerGameOver()
+    private IEnumerator TriggerGameOver()
     {
         Debug.Log("ゲームオーバー");
+
+        // ゲームオーバーの画像を表示
+        gameOverImage.gameObject.SetActive(true);
+
+        // 画像を表示して一定時間待機
+        yield return new WaitForSeconds(displayTime);
+
+        // シーン遷移
         FadeManager.Instance.LoadScene("ResultScene", 1.0f);
     }
 }
